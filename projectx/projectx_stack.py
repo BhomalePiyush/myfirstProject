0e9793c43b6cd0d652906115c0c2603303ee92a7
Piyush <piyushbhomale1999@gmail.com>
2022-02-13 22:40:25 +0530

    bucket name

--- projectx/projectx_stack.py
+++ projectx/projectx_stack.py
@@ -1,6 +1,6 @@
 from aws_cdk import (
     # Duration,
-    Stack,
+    Stack, aws_s3 as _s3
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -9,6 +9,11 @@ class ProjectxStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        _s3.Bucket(self,
+                   "myfirstbucket",
+                   bucket_name='s3newpiyusbhomale1999',
+                   versioned= True,
+                   encryption=_s3.BucketEncryption.KMS_MANAGED)
 
         # The code that defines your stack goes here
 
